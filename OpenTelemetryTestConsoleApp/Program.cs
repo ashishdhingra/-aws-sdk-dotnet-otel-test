1550d62e618b28ed09aaf46a8de29fd560d5be93
Ashish Dhingra <67916761+ashishdhingra@users.noreply.github.com>
2022-07-25 15:46:55 -0700

    Initial changes for OpenTelemetry demo app.

---
+++ OpenTelemetryTestConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.S3;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+// Define some important constants to initialize tracing with
+var serviceName = "MyCompany.MyProduct.MyService";
+var serviceVersion = "1.0.0";
+
+// Configure important OpenTelemetry settings and the console exporter
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    .AddAWSInstrumentation()
+    .AddConsoleExporter()
+    .Build();
+
+var s3Client = new AmazonS3Client();
+_ = s3Client.ListBucketsAsync().Result;
+
+var dynamoDbClient = new AmazonDynamoDBClient();
+_ = dynamoDbClient.ListTablesAsync().Result;
